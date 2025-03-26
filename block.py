60b440e238b35e4846a7884a696297fbbab18953
Arthur Touati <134126573+NFXSTUDIO@users.noreply.github.com>
2025-03-26 09:33:40 +0100

    add block.py

---
+++ block.py
@@ -0,0 +1,17 @@
+import pygame as pg
+
+class Block(Process):
+    def __init__(self,x,y,Block_map,process_name):
+        pg.sprite.Sprite.__init__(self)
+        self.image = Block_map
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.move_dir_x = 0
+        self.move_dir_y = 0
+        self.process = process_name
+    def update(self):
+        x,y = self.rect.center
+        x+= self.move_dir_x
+        y+= self.move_dir_y
+        self.rect.center = (x,y)
+
