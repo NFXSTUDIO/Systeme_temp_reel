fc433e2748f97166bbf2802fc7bfc3d42974ae52
Corentin <corentin.gaudard@gmail.com>
2025-04-21 15:11:21 +0200

    TOUT_MARCHE

--- process.py
+++ process.py
@@ -158,65 +158,57 @@ def rm_scheduling(processes):
     ready_list = [] # list that contains [pid, start_time, end_time]
     total_waiting_time = 0
     start_time = 0
-    
+    verified = 0
+
+    # time limit
     hyperperiod = max([p.period for p in processes if p.period]) * 2  # simulation jusqu'à une certaine limite
-    processes.sort(key=lambda x: x.period)  # sort per period
-    temp_pid = processes[0].pid
-    temp_st_time = processes[0].arrival_time
-    temp_time = processes[0].arrival_time
-    temp_remaining_time = processes[0].remaining_time
-
-    # Initialize the ready queue with the process that arrive at time 0
-    for p in processes:
-            if (time - p.arrival_time) % p.period == 0 and time >= p.arrival_time:
-                new_instance = Process(p.pid, time, p.burst_time, p.period)
-                heapq.heappush(ready_queue, (new_instance.period, new_instance))
-                ready_list.append([p.pid, time, -10]) # time when the process enter the ready queue
-
+    temp_pid = 0
+    
     while time < hyperperiod:
-        # Ajouter les réapparitions périodiques
-        for p in processes:
-            if (time - p.period) % p.period == 0 and time > p.arrival_time:
-                new_instance = Process(p.pid, time, p.burst_time, p.period)
-                heapq.heappush(ready_queue, (new_instance.period, new_instance))
-                ready_list.append([p.pid, time, -10])                  # time when the process enter the ready queue again
-        if ready_queue:                                             # if there is a process in the ready queue
-            _, process = heapq.heappop(ready_queue)                     # take the process in front of the list
-            ready_list.append([process.pid, -10, time])              # time when the process leave the ready queue     
-            for p in reversed(ready_list):
-                if ready_list[-1][0]==p[0] and ready_list[-1][2]==(p[2]+1):
-                    ready_list.pop()
-                    break        
-            if process.pid != temp_pid and temp_remaining_time > 0: # if the previous process is not the same as the current one
-                # print("pid remis : ",[temp_pid, temp_time, -10])
-                ready_list.insert(-1,[temp_pid, temp_time, -10])          # we mark the previous proc as finished in the ready queue
-                # print(ready_list[-2], ready_list[-1])
-                results.append([temp_pid, start_time, time])     # we add the previous one to the results
-                start_time = time                                       # set the start time to the actual time
-            if start_time == 0:
+        # arrived process                       first apparition                    periodic apparition
+        arrived = [p for p in processes if (time == p.arrival_time or (time - p.arrival_time) % p.period == 0)]
+        for p in arrived:
+            new_instance = Process(p.pid, time, p.burst_time, p.period)
+            ready_queue.append(new_instance)
+            ready_queue.sort(key=lambda x: x.period)
+            ready_list.append([new_instance.pid, time, -10])
+        # if there is process in the ready queue
+        if ready_queue:
+            process = ready_queue.pop(0)                # take the process in front of the list
+            ready_list.append([process.pid, -10, time]) # time when the process leave the ready queue     
+            # if the previous process is the same as the current one
+            if temp_pid==process.pid:
+                ready_list.pop()                        # we suppress the new instance
+            # initialize the start_time for the first process
+            if start_time == 0 and verified ==0:
                 start_time = time
-            time += 1
-            process.remaining_time -= 1
-            temp_remaining_time = process.remaining_time
-
-            if process.remaining_time == 0:                         # if the processus is finished
+                verified += 1
+            # if the process is finished
+            if process.remaining_time == 0:
                 process.turnaround_time = time - process.arrival_time
                 process.waiting_time = process.turnaround_time - process.burst_time
                 total_waiting_time += process.waiting_time
-                results.append([process.pid, start_time, time])         # we add it to the results
-                start_time = 0                                          # reset the start time
+                results.append([process.pid, start_time, time])     # we add it to the results
+                start_time = time                                   # reset the start time
+            # if there is a process with a shorter period than the curreznt one
+            elif any(p.period < process.period for p in ready_queue):
+                print("process :", process.pid, " qui est retiré, period :",process.period)
+                results.append([process.pid, start_time, time])     # we add the current one in the results
+                ready_queue.append(process)                         # we add the current one in the ready_queue
+                ready_queue.sort(key=lambda x: x.period)            # we sort the list to have the first element with the shortest period
+                ready_list.append([process.pid, -10, time])         # add the process that came back in the ready_queue
             else:
-                temp_pid = process.pid
-                temp_st_time = start_time
-                temp_time = time
-                heapq.heappush(ready_queue, (process.period, process))  
+                process.remaining_time -= 1                     # decrement the remaining time
+                ready_queue.insert(0,process)                   # add it back to the ready queue
+                temp_pid = process.pid                          # we save the current process id
+                time += 1
         else:
             time += 1
 
     performances = total_waiting_time / len(processes)
     return results, time, performances, ready_list
 
-def edf_scheduling(processes):
+def edf_scheduling(processes): 
     processes.sort(key=lambda x: x.arrival_time)
     time = 0
     results = []
@@ -224,62 +216,57 @@ def edf_scheduling(processes):
     ready_list = [] # list that contains [pid, start_time, end_time]
     total_waiting_time = 0
     start_time = 0
+    verified = 0
+
+    # time limit
+    hyperperiod = max([p.deadline for p in processes if p.period]) * 2  # simulation jusqu'à une certaine limite
+    temp_pid = 0
     
-    hyperperiod = max([p.period for p in processes if p.period]) * 2  # simulation jusqu'à une certaine limite
-    processes.sort(key=lambda x: x.period)  # sort per period
-    temp_pid = processes[0].pid
-    temp_time = processes[0].arrival_time
-    temp_remaining_time = processes[0].remaining_time
-
-    # Initialize the ready queue with the process that arrive at time 0
-    for p in processes:
-            if (time - p.arrival_time) % p.period == 0 and time >= p.arrival_time:
-                new_instance = Process(p.pid, time, p.burst_time, p.period, p.deadline+time)
-                heapq.heappush(ready_queue, (new_instance.deadline, new_instance))
-                ready_list.append([p.pid, time, -10]) # time when the process enter the ready queue
-
     while time < hyperperiod:
-        # add periodicly the processes
-        for p in processes:
-            if (time - p.period) % p.period == 0 and time > p.arrival_time:
-                new_instance = Process(p.pid, time, p.burst_time, p.period, p.deadline+time)
-                heapq.heappush(ready_queue, (new_instance.deadline, new_instance))
-                ready_list.append([p.pid, time, -10])                  # time when the process enter the ready queue again
-        # Remove expired processes
-        ready_queue = [(d, p) for (d, p) in ready_queue if time < p.deadline]
-        heapq.heapify(ready_queue)
-
-        # if there is a readyqueue
-        if ready_queue:                                             # if there is a process in the ready queue
-            _, process = heapq.heappop(ready_queue)                     # take the process in front of the list
-            ready_list.append([process.pid, -10, time])              # time when the process leave the ready queue     
-            for p in reversed(ready_list):
-                if ready_list[-1][0]==p[0] and ready_list[-1][2]==(p[2]+1):
-                    ready_list.pop()
-                    break 
-            # if the previous process is not the same as the current one       
-            if process.pid != temp_pid and temp_remaining_time > 0: 
-                ready_list.insert(-1,[temp_pid, temp_time, -10])        # we mark the previous proc as finished in the ready queue
-                results.append([temp_pid, start_time, time])     # we add the previous one to the results
-                start_time = time                                       # set the start time to the actual time
-            # if the start time has been reset
-            if start_time == 0:
-                start_time = time   # set to the current time
-
-            time += 1
-            process.remaining_time -= 1
-            temp_remaining_time = process.remaining_time
+        # arrived process                       first apparition                    periodic apparition
+        arrived = [p for p in processes if (time == p.arrival_time or (time - p.arrival_time) % p.period == 0)]
+        for p in arrived:
+            new_instance = Process(p.pid, time, p.burst_time, p.period, p.deadline + time)
+            ready_queue.append(new_instance)
+            ready_queue.sort(key=lambda x: x.deadline)
+            ready_list.append([new_instance.pid, time, -10])
+        # if there is process in the ready queue
+        if ready_queue:
+            # loop to look for a process 
+            for p in ready_queue:
+                # if the process reaches its deadline
+                if p.deadline == time:
+                    ready_queue.remove(p)               # we erase it
+                    ready_list.append([p.pid, -10, time])#we mark it as leaving the queue
+            
+            process = ready_queue.pop(0)                # take the process in front of the list
+            ready_list.append([process.pid, -10, time]) # time when the process leave the ready queue     
+            # if the previous process is the same as the current one
+            if temp_pid==process.pid:
+                ready_list.pop()                        # we suppress the new instance
+            # initialize the start_time for the first process
+            if start_time == 0 and verified == 0:
+                start_time = time
+                verified += 1
             # if the process is finished
-            if process.remaining_time == 0:                         # if the processus is finished
+            if process.remaining_time == 0:
                 process.turnaround_time = time - process.arrival_time
                 process.waiting_time = process.turnaround_time - process.burst_time
                 total_waiting_time += process.waiting_time
-                results.append([process.pid, start_time, time])         # we add it to the results
-                start_time = 0                                        # reset the start time    
+                results.append([process.pid, start_time, time])     # we add it to the results
+                start_time = time                                   # reset the start time
+            # if there is a process with a shorter period than the curreznt one
+            elif any(p.deadline < process.deadline for p in ready_queue):
+                print("process :", process.pid, " qui est retiré, deadline :",process.deadline)
+                results.append([process.pid, start_time, time])     # we add the current one in the results
+                ready_queue.append(process)                         # we add the current one in the ready_queue
+                ready_queue.sort(key=lambda x: x.deadline)            # we sort the list to have the first element with the shortest period
+                ready_list.append([process.pid, -10, time])         # add the process that came back in the ready_queue
             else:
-                temp_pid = process.pid
-                temp_time = time
-                heapq.heappush(ready_queue, (process.period, process))  
+                process.remaining_time -= 1                     # decrement the remaining time
+                ready_queue.insert(0,process)                   # add it back to the ready queue
+                temp_pid = process.pid                          # we save the current process id
+                time += 1
         else:
             time += 1
 
@@ -322,9 +309,9 @@ if __name__ == "__main__":
     print("Average Waiting Time:", rr_avg_wait)
     
     processes1 = [
-        Process(1, 0, 1, 4),
-        Process(2, 0, 2, 5),  
-        Process(3, 0, 3, 10),  
+        Process(1, 1, 1, 5),
+        Process(2, 1, 2, 4),  
+        Process(3, 1, 3, 10),  
     ]
     print("\nRM Scheduling:")
     rm_results, rm_tt_time, rm_avg_wait, rd_list = rm_scheduling(processes1)
